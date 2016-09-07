466c296d586a233d1514315c361f08559b7e180b
Jens Kutilek <webmail@kutilek.de>
2016-09-07 15:53:24 +0200

    Fix "Select Glyphs With Errors"

--- RedArrow.roboFontExt/lib/Select Glyphs With Errors.py
+++ RedArrow.roboFontExt/lib/Select Glyphs With Errors.py
@@ -5,10 +5,14 @@ from lib.tools.defaults import getDefault
 
 options = {
     "extremum_calculate_badness": True,
-    "extremum_ignore_badness_below": 2,
-    "smooth_connection_max_distance": 4,
     "fractional_ignore_point_zero": True,
+    
+    "extremum_ignore_badness_below": 0,
+    "smooth_connection_max_distance": 4,
     "collinear_vectors_max_distance": 2,
+    "semi_hv_vectors_min_distance": 30,
+    "zero_handles_max_distance": 0,
+    
     "grid_length": getDefault('glyphViewRoundValues'),
 }
 
@@ -17,7 +21,7 @@ def run_test(font, glyphnames):
     for n in glyphnames:
         g = font[n]
         otp = OutlineTestPen(CurrentFont(), options)
-        g.draw(otp)
+        g.drawPoints(otp)
         if otp.errors:
             #if len(otp.errors) > 0:
             #    g.mark = (1, 0.65, 0.6, 1)
