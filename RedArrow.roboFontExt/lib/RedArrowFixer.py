db29e5a65120202afafc9a816245f7db0e6c21b1
Jens Kutilek <webmail@kutilek.de>
2021-06-11 12:15:10 +0200

    Format with black

--- RedArrow.roboFontExt/lib/RedArrowFixer.py
+++ RedArrow.roboFontExt/lib/RedArrowFixer.py
@@ -2,8 +2,10 @@ from __future__ import print_function, division
 from AppKit import NSBezierPath, NSImage
 from os.path import join, dirname, isfile
 from fontTools.misc.arrayTools import pointInRect
-from lib.tools import bezierTools # for single point click selection
-from lib.tools.defaults import getDefaultColor # for drawing the selection marquee
+from lib.tools import bezierTools  # for single point click selection
+from lib.tools.defaults import (
+    getDefaultColor,
+)  # for drawing the selection marquee
 from mojo.events import BaseEventTool, installTool
 from mojo.drawingTools import fill, oval, restore, save, stroke
 
@@ -11,6 +13,7 @@ DEBUG = True
 
 if DEBUG:
     import RedArrow
+
     reload(RedArrow)
 from RedArrow import RedArrowUI
 
@@ -23,76 +26,82 @@ else:
     toolbarIcon = None
     print("Warning: Toolbar icon not found at path: '%s'" % iconpath)
 
+
 class ErrorSelection(object):
     def __init__(self):
         self.errors = []
-    
+
     def __repr__(self):
         return str(self.errors)
-    
+
     def resetSelection(self):
         self.errors = []
-    
+
     def addError(self, p, shiftDown):
-        #if contour in self.errors:
+        # if contour in self.errors:
         #    if not p in self.errors[contour]:
         #        self.errors.append((p, contour))
-        #else:
+        # else:
         #    self.errors[contour] = [p]
         if not p in self.errors:
             self.errors.append(p)
-    
+
     def draw(self, scale):
         size = 20 * scale
         save()
         fill(0.2, 0.5, 0.35, 0.3)
         stroke(None)
-        #for contour in self.errors.keys():
+        # for contour in self.errors.keys():
         #    for p in contour:
         #        oval(p[0]-5, p[1]-5, 10, 10)
         for p in self.errors:
-            oval(p[0]-size/2, p[1]-size/2, size, size)
+            oval(p[0] - size / 2, p[1] - size / 2, size, size)
         restore()
 
+
 class RedArrowFixerTool(BaseEventTool):
-    
     def setup(self):
         self.errorSelection = ErrorSelection()
         self.pStart = None
         self.pEnd = None
         self._selectedMouseDownPoint = None
-    
+
     def getToolbarIcon(self):
         return toolbarIcon
-    
+
     def getToolbarTip(self):
         return "Red Arrow Fixer"
-    
+
     def becomeActive(self):
         if DEBUG:
             print("becomeActive")
         self.ui = RedArrowUI()
         self.ui.fixer = self
-    
+
     def becomeInactive(self):
         if DEBUG:
             print("becomeInactive")
         self.ui.windowCloseCallback(None)
         self.ui.w.close()
-    
+
     def fixSelected(self):
         if DEBUG:
             print("fixSelected")
         # Add an anchor at position p
         g = CurrentGlyph()
-        #g.prepareUndo(undoTitle="Add anchor %s to /%s" % (newAnchorName, g.name))
+        # g.prepareUndo(undoTitle="Add anchor %s to /%s" % (newAnchorName, g.name))
         # TODO Fix problems
-        #g.performUndo()
-    
+        # g.performUndo()
+
     def _normalizeBox(self, p0, p1):
         # normalize selection rectangle so it is always positive
-        return (min(p0.x, p1.x), min(p0.y, p1.y), max(p0.x, p1.x), max(p0.y, p1.y))
-    
+        return (
+            min(p0.x, p1.x),
+            min(p0.y, p1.y),
+            max(p0.x, p1.x),
+            max(p0.y, p1.y),
+        )
+
     def _getSelectedPoints(self):
         if self.pStart and self.pEnd:
             box = self._normalizeBox(self.pStart, self.pEnd)
@@ -101,12 +110,12 @@ class RedArrowFixerTool(BaseEventTool):
                 if pointInRect(p, box):
                     self.errorSelection.addError(p, self.shiftDown)
                     self._selectedMouseDownPoint = p
-    
+
     def mouseDown(self, point, clickCount):
-        if not(self.shiftDown):
+        if not (self.shiftDown):
             self.errorSelection.resetSelection()
         if clickCount > 1:
-            #self._newAnchor(point)
+            # self._newAnchor(point)
             pass
         else:
             self.pStart = point
@@ -118,32 +127,33 @@ class RedArrowFixerTool(BaseEventTool):
                     self.errorSelection.addError(p, self.shiftDown)
                     self._selectedMouseDownPoint = p
                     break
-    
+
     def mouseUp(self, point):
         self.pEnd = point
         self._getSelectedPoints()
-        #print(self.errorSelection)
+        # print(self.errorSelection)
         self.pStart = None
         self.pEnd = None
         self._selectedMouseDownPoint = None
-    
+
     def mouseDragged(self, point, delta):
         self.pEnd = point
-        #self._getSelectedPoints()
-    
+        # self._getSelectedPoints()
+
     def draw(self, scale):
         if self.isDragging() and self.pStart and self.pEnd:
             r = self.getMarqueRect()
             if r:
-                color = getDefaultColor('glyphViewSelectionMarqueColor')
+                color = getDefaultColor("glyphViewSelectionMarqueColor")
                 color.set()
                 path = NSBezierPath.bezierPathWithRect_(r)
                 path.fill()
             return
         self.drawSelection(scale)
-    
+
     def drawSelection(self, scale):
         self.errorSelection.draw(scale)
-        
+
+
 installTool(RedArrowFixerTool())
 print("Red Arrow Fixer installed in tool bar.")
--- RedArrow.roboFontExt/lib/Select Glyphs With Errors.py
+++ RedArrow.roboFontExt/lib/Select Glyphs With Errors.py
@@ -1,21 +1,21 @@
 from __future__ import print_function, division
-#import outlineTestPen
-#reload(outlineTestPen)
+
+# import outlineTestPen
+# reload(outlineTestPen)
 from outlineTestPen import OutlineTestPen
 from lib.tools.defaults import getDefault
 
 options = {
     "extremum_calculate_badness": True,
     "fractional_ignore_point_zero": True,
-    
     "extremum_ignore_badness_below": 0,
     "smooth_connection_max_distance": 4,
     "collinear_vectors_max_distance": 2,
     "semi_hv_vectors_min_distance": 30,
     "zero_handles_max_distance": 0,
-    
-    "grid_length": getDefault('glyphViewRoundValues'),
+    "grid_length": getDefault("glyphViewRoundValues"),
 }
+
 
 def run_test(font, glyphnames):
     selection = []
@@ -27,6 +27,7 @@ def run_test(font, glyphnames):
         if otp.errors:
             selection.append(g.name)
     font.selection = selection
+
 
 font = CurrentFont()
 glyphnames = CurrentFont().keys()
